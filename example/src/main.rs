2a706f4ef7da8c8f7299133275b841cc2f65d104
Fedor Gogolev <knsd@knsd.net>
2018-04-11 21:55:16 +0300

    docs: update example

--- example/src/main.rs
+++ example/src/main.rs
@@ -2,5 +2,15 @@
 
 fn main() {
     let info = buildinfo!();
+
+    println!("Target triple: {}", info.target_triple());
+    println!("Host triple: {}", info.host_triple());
+    println!("Opt level: {}", info.opt_level());
+    println!("Debug: {}", info.debug());
+    println!("Profile: {}", info.profile());
+    println!("Rustc version: {}", info.rustc_version());
+    println!("Compiled_at: {:?}", info.compiled_at());
+    println!("Git commit: {:?}", info.git_commit());
+
     println!("{:?}", info)
 }
